e7cc07581f1b5e8c43bc4ea02b2597c6865087c7
Hakxel <asiel_montes@yahoo.com>
2018-12-23 12:03:13 -0500

    Create basic web server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+
+app.use(express.static(__dirname + 'client/build'));
+
+
+
+let PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => { console.log("App is running on port " + PORT)})

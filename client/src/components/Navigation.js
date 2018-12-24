68b9bbefe64ad01eecf8cfdcaecbdf4c38d77335
Hakxel <asiel_montes@yahoo.com>
2018-12-24 13:14:37 -0500

    Finish initial header and footer style

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navigation.css'
 
 
 const Navigation = () => {
   return(
-    <div className="ui menu inverted blue">
+    <div className="ui borderless menu inverted blue" id="navigation">
+      <div className="right fluid item menu">
       <Link to="/" className="header item">Home</Link>
       <Link to="/about" className="item">About</Link>
+
+      </div>
     </div>
   )
 }

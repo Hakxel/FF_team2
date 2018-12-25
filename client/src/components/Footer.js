c7898c44052388e61aa279e996f225112afc669c
Hakxel <asiel_montes@yahoo.com>
2018-12-25 14:16:18 -0500

    Add basic text and menus

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './Footer.css'
+import '../styles/Footer.css'
 
 
 const Footer = () => {
   return (
     <div className="footer">
-      <div class="ui borderless menu inverted blue footer">
+      <div class="ui borderless menu inverted blue">
         <a href="https://github.com/Hakxel/FF_team2" target="_blank">
           <i class="black huge github square icon"></i>      
         </a>
--- client/src/components/MainContent.js
+++ client/src/components/MainContent.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import '../styles/MainContent.css'
 
 const MainContent = () => {
   return (
-    <div className="MainContent">
-      <h1>Welcome</h1>
+    <div className="main-content">
+      <div className="hero">
+        <div>
+        <h1>Bitworld Shopper</h1>
+        <h2>A global currency, a world of possibilities...</h2>
+        </div>
+        <div className="where-to-spend">
+          <h2>Where can I spend bitcoin?</h2>
+
+          <div className="spend-menu ui top pointing menu">
+            <a className="active item left">Local stores</a>
+            <a className="item right">Online stores</a>
+          </div>
+          <div className="spend-search">
+
+            <div className="local-search">
+
+              <div class="ui transparent icon left labeled input">
+                <div class="ui dropdown label">
+                  <div class="text">City</div>
+                  <i class="dropdown icon"></i>
+                  <div class="menu">
+                    <div class="item">City</div>
+                    <div class="item">State</div>
+                    <div class="item">Country</div>
+                  </div>
+                </div>
+                <input type="text" placeholder="Search..." />
+                  <i class="search link icon"></i>
+              </div>
+
+            </div>
+            
+            <div className="online-search">
+              <div class="ui transparent icon input">         
+                <input type="text" placeholder="Search..." />
+                  <i class="search link icon"></i>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+      </div>
     </div>
   )
 }

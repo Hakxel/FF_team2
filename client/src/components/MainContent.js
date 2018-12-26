b2ec7c5acd8a061dd68ea7999208c4696b1184e5
Hakxel <asiel_montes@yahoo.com>
2018-12-26 14:25:59 -0500

    Fix axios request

--- client/src/components/MainContent.js
+++ client/src/components/MainContent.js
@@ -1,53 +1,73 @@
 import React from 'react';
 import '../styles/MainContent.css'
+import axios from 'axios';
 
-const MainContent = () => {
-  return (
-    <div className="main-content">
-      <div className="hero">
-        <div>
-        <h1>Bitworld Shopper</h1>
-        <h2>A global currency, a world of possibilities...</h2>
-        </div>
-        <div className="where-to-spend">
-          <h2>Where can I spend bitcoin?</h2>
+class MainContent extends React.Component {
+  constructor() {
+   super();
+   this.state = {
+     venues: {},
+     onlineStores : {}
+   } 
+  }
 
-          <div className="spend-menu ui top pointing menu">
-            <a className="active item left">Local stores</a>
-            <a className="item right">Online stores</a>
+  async componentDidMount(){
+    let { data } = await axios.get("http://cors-anywhere.herokuapp.com/coinmap.org/api/v1/venues/")
+    this.setState({
+      venues: data,
+    })
+    console.log(data)
+    // console.log(this.venues)
+  }
+
+  render(){
+    return (
+      <div className="main-content">
+        <div className="hero">
+          <div>
+          <h1>Bitworld Shopper</h1>
+          <h2>A global currency, a world of possibilities...</h2>
           </div>
-          <div className="spend-search">
-
-            <div className="local-search">
-
-              <div class="ui transparent icon left labeled input">
-                <div class="ui dropdown label">
-                  <div class="text">City</div>
-                  <i class="dropdown icon"></i>
-                  <div class="menu">
-                    <div class="item">City</div>
-                    <div class="item">State</div>
-                    <div class="item">Country</div>
+          <div className="where-to-spend">
+            <h2>Where can I spend bitcoin?</h2>
+  
+            <div className="spend-menu ui top pointing menu">
+              <a className="active item left">Local stores</a>
+              <a className="item right">Online stores</a>
+            </div>
+            <div className="spend-search">
+  
+              <div className="local-search">
+  
+                <div className="ui transparent icon left labeled input">
+                  <div className="ui dropdown label">
+                    <div className="text">City</div>
+                    <i className="dropdown icon"></i>
+                    <div className="menu">
+                      <div className="item">City</div>
+                      <div className="item">State</div>
+                      <div className="item">Country</div>
+                    </div>
                   </div>
+                  <input type="text" placeholder="Search..." />
+                  <i className="search link icon"></i>
                 </div>
-                <input type="text" placeholder="Search..." />
-                  <i class="search link icon"></i>
+  
               </div>
-
+              
+              <div className="online-search">
+                <div className="ui transparent icon input">     
+                  <input type="text" placeholder="Search..." />
+                  <i className="search link icon"></i>
+                </div>
+              </div>
             </div>
             
-            <div className="online-search">
-              <div class="ui transparent icon input">         
-                <input type="text" placeholder="Search..." />
-                  <i class="search link icon"></i>
-              </div>
-            </div>
           </div>
-          
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default MainContent;

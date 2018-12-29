43db51f11fcb4387c129d5d47d890ff84e168456
Alvaro Vargas <vargasa910@gmail.com>
2018-12-29 14:35:09 -0500

    Search Results styles update, reversed values and eliminated whitespace

--- client/src/components/SearchResults.js
+++ client/src/components/SearchResults.js
@@ -19,10 +19,12 @@ class SearchResults extends React.Component {
       // <div><p>{this.props.product.name}</p></div>
         <div className="product">
           <div id="thumbnail">
-            <img src={this.props.product.thumbnailURL} alt={this.props.product.name} />
+            <img src={this.props.product.thumbnailURL} alt="photo not available" />
+          </div>
+          <div id="product-name">
+            <p>{this.props.product.name}</p>
           </div>
           <div className="product-detail">
-            <p id="product-name">{this.props.product.name}</p>
             <p id="product-info">Merchant: {this.props.product.merchantName}</p>
             <p id="product-info">Price(USD): {this.props.product.priceAsString}</p>
             <a href={this.props.product.url} id="product-info">Buy it with Bitcoin!</a>

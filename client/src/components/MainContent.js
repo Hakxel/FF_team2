4417b306e005dbbaf6f9275bc17c95e2a631cda2
JamieDamon <happyj77@hotmail.com>
2018-12-31 12:03:23 -0500

    fixed npm issues

--- client/src/components/MainContent.js
+++ client/src/components/MainContent.js
@@ -11,14 +11,8 @@ class MainContent extends React.Component {
    this.state = {
      products: false,
      searchValue: ''
-<<<<<<< HEAD
    }
   }
-=======
-   } 
-  } 
-  
->>>>>>> asiel-home
 
   fetchProductData = async () => {
   let { data } = await axios.get(`http://cors-anywhere.herokuapp.com/spendabit.co/api/v0/go?q=${this.state.searchValue}`)
@@ -62,15 +56,11 @@ class MainContent extends React.Component {
           </div>
           <div className="what-to-buy">
             <h2>What can I buy with bitcoin?</h2>
-<<<<<<< HEAD
             <form className="search-bar ui large fluid action left icon input" onSubmit={this.handleSubmit}>
               <i className="search icon"></i>
               <input type="text" placeholder="Search for product..." name="test" />
               <button className="ui blue button">Search</button>
             </form>
-=======
-            <SearchForm onSubmit={this.handleSubmit} searchValue={this.state.searchValue} handleChange={this.handleChange} handleSubmit={this.handleSubmit}/>           
->>>>>>> asiel-home
           </div>
         </div>
         {this.renderResults()}        

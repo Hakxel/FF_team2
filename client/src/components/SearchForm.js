b82ec601167343527272777edefadc057407e1da
Hakxel <asiel_montes@yahoo.com>
2019-01-01 22:16:24 -0500

    Reduce font size on product search page

--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SearchForm = (props) => {
   return(
 
-    <form className="search-bar ui fluid left icon input" onSubmit={props.handleSubmit}>
+    <form className="search-bar ui large fluid left icon input" onSubmit={props.handleSubmit}>
       <i className="search icon"></i>
       <input type="text" placeholder="Search by keywords..." name="productSearch" value={props.searchValue} onChange={props.handleChange}/>
       <button className="ui blue button">Search</button>

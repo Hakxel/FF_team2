39accc24aee5b47bc9eb6d05f80889263a89009f
Hakxel <asiel_montes@yahoo.com>
2019-02-21 17:38:43 -0500

    include build in push

---
+++ client/build/precache-manifest.84d7c6f8255c334372bb223d9a8e6f67.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb53112810a2f144ccdfb810669b2224",
+    "url": "/static/media/bitglobe.bb531128.jpg"
+  },
+  {
+    "revision": "3311e02fb5ef281931a60a3bce15fd38",
+    "url": "/static/media/artist-impression.3311e02f.jpg"
+  },
+  {
+    "revision": "a0db966332aff0b296928f9c4cc14831",
+    "url": "/static/media/Vp3BX.a0db9663.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "3552ec9451112cddb0e9",
+    "url": "/static/js/main.3552ec94.chunk.js"
+  },
+  {
+    "revision": "1bb2a5a47bf21d7989fb",
+    "url": "/static/js/2.1bb2a5a4.chunk.js"
+  },
+  {
+    "revision": "3552ec9451112cddb0e9",
+    "url": "/static/css/main.7cc51536.chunk.css"
+  },
+  {
+    "revision": "560d8cc7c8a5aa4e1c1be62134de7097",
+    "url": "/index.html"
+  }
+];

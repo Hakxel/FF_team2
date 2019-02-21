604c84ee42b1102ebf36595b6c742aa43b2ad0f7
Hakxel <asiel_montes@yahoo.com>
2019-02-21 17:25:19 -0500

    change express.static

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 const app = express();
 
-app.use(express.static(__dirname + 'client/build/static'));
+app.use(express.static(__dirname + 'client/build/'));
 
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/client/build/index.html`)

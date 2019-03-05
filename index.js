7a789c8398a0b56cc470e180a32e015ab2b85b98
Hakxel <asiel_montes@yahoo.com>
2019-03-05 09:12:59 -0500

    Include client folder in path

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const path = require('path')
 
 const app = express();
 
-app.use(express.static( path.join(__dirname, 'build')));
+app.use(express.static( path.join(__dirname, 'client/build')));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 
 app.get('/localsearch', async (req, res) => {

716fc9bdd5de942aa5f683fcb1c00686c949b3e8
Hakxel <asiel_montes@yahoo.com>
2019-03-05 14:48:13 -0500

    Modify server path

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.static( path.join(__dirname, 'client/build')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'))
 })
 

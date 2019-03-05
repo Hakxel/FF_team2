2f27b72d807af324cfd65c6c41561a57e8dc8c62
Hakxel <asiel_montes@yahoo.com>
2019-03-05 12:29:53 -0500

    Use resolve in express server

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const app = express();
 
 app.use(express.static( path.join(__dirname, 'client/build')));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'))
 })
 
 app.get('/localsearch', async (req, res) => {

7d5a3cea0d33f9540c3e87b60649cd2b76ab380f
Hakxel <asiel_montes@yahoo.com>
2019-03-05 09:05:14 -0500

    Modify server rendering path

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const axios = require('axios');
+const path = require('path')
 
 const app = express();
 
-app.use(express.static(__dirname + 'client/build/'));
+app.use(express.static( path.join(__dirname, 'build')));
 
-app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/client/build/index.html`)
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
 app.get('/localsearch', async (req, res) => {

d5444592fd6f3c8c345777d62aa557459c170f75
Hakxel <asiel_montes@yahoo.com>
2019-03-06 10:26:41 -0500

    Create and use local build

---
+++ client/build/precache-manifest.dca0efc1c0520343c5ccac3f1ecb0e3f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb53112810a2f144ccdfb810669b2224",
+    "url": "/static/media/bitglobe.bb531128.jpg"
+  },
+  {
+    "revision": "3311e02fb5ef281931a60a3bce15fd38",
+    "url": "/static/media/artist-impression.3311e02f.jpg"
+  },
+  {
+    "revision": "a0db966332aff0b296928f9c4cc14831",
+    "url": "/static/media/Vp3BX.a0db9663.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f1fd67c3d3a5123d6c7e",
+    "url": "/static/js/main.f1fd67c3.chunk.js"
+  },
+  {
+    "revision": "1bb2a5a47bf21d7989fb",
+    "url": "/static/js/2.1bb2a5a4.chunk.js"
+  },
+  {
+    "revision": "f1fd67c3d3a5123d6c7e",
+    "url": "/static/css/main.7cc51536.chunk.css"
+  },
+  {
+    "revision": "259528c21569c82ed1583fd554a1d95a",
+    "url": "/index.html"
+  }
+];

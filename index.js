1dc469a3fc873022770ae21e2cd57da91e54adcc
Hakxel <asiel_montes@yahoo.com>
2019-03-06 07:30:40 -0500

    Modify external api paths

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ app.get('/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client/build/public', 'index.html'))
 })
 
-app.get('/localsearch', async (req, res) => {
+app.get('*/localsearch', async (req, res) => {
   let { data } = await axios.get("http://coinmap.org/api/v1/venues/");
   res.send(data.venues)
 })
 
-app.get('/onlinesearch/:productName', async (req, res) => {
+app.get('*/onlinesearch/:productName', async (req, res) => {
   let product = req.params.productName
   let { data } = await axios.get("http://spendabit.co/api/v0/go?q=" + product);
   res.send(data.products)
